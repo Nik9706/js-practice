83a98095f2d33d200b512ae275ba556bcc0c38c3
Nitesh Dwivedi <67825569+Nik9706@users.noreply.github.com>
2025-06-04 15:22:09 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as newer version 
+// alert(3+3) we are using nodejs not browser
+
+// console.log(3
+// +3) code rediability 
+
+// ECMA - //tc39.es  documentation 
+
+
+/* Types of data type ;- 
+Primitive data Types -
+
+number => 2 to power 53
+bigint =. Generally used in making large apps (stock fb etc apps )
+string =>""
+boolean =>true/false
+null => standalone value ( we use while representing temp) null got type as object 
+undefined=> 
+symbol => unique
+
+object
+*/
+console.log(typeof "Nitesh Dwived")
+
+console.log(typeof null)    // Object its an interview question 
+console.log(typeof undefined)
+console.log(typeof Symbol)
+
+//Non Primitive Data types 
+// object Array Function 
+
+
+
+
