d8a5946d8e6c8f383668f9d809e92ee789de3567
Nitesh Dwivedi <67825569+Nik9706@users.noreply.github.com>
2025-06-04 15:22:38 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId= "12121"
+let accountemail= "nitesh.kumar@gmail.com"
+var accountname="nik dwivedi"
+accountaddress = "delhi";
+let accountcity;
+
+// accountId="2132"
+ accountemail="surya.kumar@gmail.com";
+var accountname="kunal sharma"
+
+console.table([accountId,accountemail,accountname,accountaddress,accountcity])
+
+/* always prefer to use the let variables due to block and fucntional scope */
